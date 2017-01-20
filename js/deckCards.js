d8db0db7a9085462f538904a6e965824197d68f6
Antonio Ruballos <ant.ruballos@gmail.com>
2017-01-19 22:16:00 -0800

    Add cards for Nilfgaard and Scoiaetel decks

--- js/deckCards.js
+++ js/deckCards.js
@@ -192,3 +192,406 @@ gwent.northernCards = [
 		ability: "Tight Bond"
 	}
 ];
+
+gwent.nilfgaardCards = [
+	{
+		name: "The Relentless",
+		type: "LeaderCard",
+		ability: "Draw from Enemy Discard"
+	},
+	{
+		name: "The White Flame Dancing on the Graves of His Foes",
+		type: "LeaderCard",
+		ability: "Cancel Leader"
+	},
+	{
+		name: "His Imperial Majesty",
+		type: "LeaderCard",
+		ability: "Rain"
+	},
+	{
+		name: "The Emperor of Nilfgaard",
+		type: "LeaderCard",
+		ability: "Look at 3 cards"
+	},	
+	{
+		name: "Letho of Gulet",
+		type: "CombatCard",
+		position: "Melee",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Menno Coehoorn",
+		type: "CombatCard",
+		position: "Melee",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Morvran Voorhis",
+		type: "CombatCard",
+		position: "Siege",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Tibor Eggebracht",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 2,
+		ability: ""
+	},
+	{
+		name: "Albrich",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 2,
+		ability: ""
+	},
+	{
+		name: "Assire var Anahid",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Cynthia",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 4,
+		ability: ""
+	},
+	{
+		name: "Fringilla Vigo",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Morteisen",
+		type: "CombatCard",
+		position: "Melee",
+		score: 3,
+		ability: ""
+	},
+	{
+		name: "Rainfarn",
+		type: "CombatCard",
+		position: "Melee",
+		score: 4,
+		ability: ""
+	},
+	{
+		name: "Renauld aep Matsen",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 5,
+		ability: ""
+	},
+	{
+		name: "Rotten Mangoel",
+		type: "CombatCard",
+		position: "Siege",
+		score: 3,
+		ability: ""
+	},
+	{
+		name: "Shilard Fitz-Oesterlen",
+		type: "CombatCard",
+		position: "Melee",
+		score: 7,
+		ability: "Spy"
+	},
+	{
+		name: "Stefan Skellen",
+		type: "CombatCard",
+		position: "Melee",
+		score: 9,
+		ability: "Spy"
+	},
+	{
+		name: "Sweers",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 2,
+		ability: ""
+	},
+	{
+		name: "Vanhemar",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 4,
+		ability: ""
+	},
+	{
+		name: "Vattier de Rideaux",
+		type: "CombatCard",
+		position: "Melee",
+		score: 4,
+		ability: "Spy"
+	},
+	{
+		name: "Vreemde",
+		type: "CombatCard",
+		position: "Melee",
+		score: 2,
+		ability: ""
+	},
+	{
+		name: "Cahir Mawr Dyffryn aep Ceallach",
+		type: "CombatCard",
+		position: "Melee",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Puttkammer",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 3,
+		ability: ""
+	},
+	{
+		name: "Archer Support",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 1,
+		ability: "Medic"
+	},
+	{
+		name: "Black Infantry Archer",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 10,
+		ability: ""
+	},
+	{
+		name: "Siege Support",
+		type: "CombatCard",
+		position: "Siege",
+		score: 0,
+		ability: "Medic"
+	},
+	{
+		name: "Heavy Zerrikanian Fire Scorpion",
+		type: "CombatCard",
+		position: "Siege",
+		score: 10,
+		ability: ""
+	},
+	{
+		name: "Zerrikanian Fire Scorpion",
+		type: "CombatCard",
+		position: "Siege",
+		score: 5,
+		ability: ""
+	},
+	{
+		name: "Impera Brigade",
+		type: "CombatCard",
+		position: "Melee",
+		score: 3,
+		ability: "Tight Bond"
+	},
+	{
+		name: "Nausicaa Cavalry Brigade",
+		type: "CombatCard",
+		position: "Melee",
+		score: 2,
+		ability: "Tight Bond"
+	},
+	{
+		name: "Combat Engineer",
+		type: "CombatCard",
+		position: "Siege",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Young Emissary",
+		type: "CombatCard",
+		position: "Melee",
+		score: 5,
+		ability: "Tight Bond"
+	}
+];
+
+gwent.scoiataelCards = [
+	{
+		name: "Queen of Dol Blathanna",
+		type: "LeaderCard",
+		ability: "Scorch Melee"
+	},
+	{
+		name: "The Beautiful",
+		type: "LeaderCard",
+		ability: "Horn Ranged"
+	},
+	{
+		name: "Daisy of the Valley",
+		type: "LeaderCard",
+		ability: "Draw 1 Card"
+	},
+	{
+		name: "Pureblood Elf",
+		type: "LeaderCard",
+		ability: "Frost"
+	},	
+	{
+		name: "Eithne",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Saskia/Saesenthessis",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Isengrim Faoiltiarna",
+		type: "CombatCard",
+		position: "Melee",
+		score: 10,
+		ability: "Morale Boost"
+	},
+	{
+		name: "Iorveth",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 10,
+		ability: "Hero"
+	},
+	{
+		name: "Dennis Cramer",
+		type: "CombatCard",
+		position: "Melee",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Milva",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 10,
+		ability: "Morale Boost"
+	},
+	{
+		name: "Ida Emean",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 6,
+		ability: ""
+	},
+	{
+		name: "Filavandrel",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 6,
+		ability: "Agile"
+	},
+	{
+		name: "Yaevinn",
+		type: "CombatCard",
+		position: "Melee",
+		score: 6,
+		ability: "Agile"
+	},
+	{
+		name: "Toruviel",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 2,
+		ability: ""
+	},
+	{
+		name: "Riordain",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 1,
+		ability: ""
+	},
+	{
+		name: "Ciaran aep Easnillien",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 3,
+		ability: "Agile"
+	},
+	{
+		name: "Barclay Els",
+		type: "CombatCard",
+		position: "Melee",
+		score: 6,
+		ability: "Agile"
+	},
+	{
+		name: "Hav'caaren Medic",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 0,
+		ability: "Medic"
+	},
+	{
+		name: "Vrihedd Brigade",
+		type: "CombatCard",
+		position: "Melee",
+		score: 5,
+		ability: "Agile"
+	},
+	{
+		name: "Dol Blathanna Scout",
+		type: "CombatCard",
+		position: "Melee",
+		score: 6,
+		ability: "Agile"
+	},
+	{
+		name: "Dwarf Skirmisher",
+		type: "CombatCard",
+		position: "Melee",
+		score: 3,
+		ability: "Muster"
+	},
+	{
+		name: "Mahakaman Defenders",
+		type: "CombatCard",
+		position: "Melee",
+		score: 5,
+		ability: ""
+	},
+	{
+		name: "Elf Skirmisher",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 2,
+		ability: "Muster"
+	},
+	{
+		name: "Vrihedd Cadet",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 4,
+		ability: ""
+	},
+	{
+		name: "Dol Blathanna Archer",
+		type: "CombatCard",
+		position: "Ranged",
+		score: 4,
+		ability: ""
+	},
+	{
+		name: "Hav'caaren",
+		type: "CombatCard",
+		position: "Melee",
+		score: 5,
+		ability: "Muster"
+	}
+];
