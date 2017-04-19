e6bf02015564a68014114593fd4b75889b359177
Antonio Ruballos <ant.ruballos@gmail.com>
2017-04-18 20:26:47 -0700

    Install latest version of React-Router-Dom and add it to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import MainContainer from './Containers/MainContainer.jsx';
 import store from './Store/Store.js';
 import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-  	<MainContainer />
-  </Provider>,
-  document.getElementById('root')
+
+	<Provider store={store}>
+		<HashRouter>
+			<Route path="/" component={MainContainer}/>
+		</HashRouter>
+	</Provider>,
+	document.getElementById('root')
 );
